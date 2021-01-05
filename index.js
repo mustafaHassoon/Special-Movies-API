cea15f25cc29ee487bb8d1f8da07a3af0ac77d50
mustafaHassoon <mustafahassoon@gmail.com>
2021-01-05 12:31:31 +0100

    implementing input validation

--- index.js
+++ index.js
@@ -164,7 +164,13 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) 
 // Users
 
 //Add a user
-app.post('/users', (req, res) => {
+app.post('/users',
+[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], (req, res) => {
   let hashedPassword = Users.hashPassword(req.body.Password);
 	Users.findOne({ Username: req.body.Username })
 	  .then((user) => {
